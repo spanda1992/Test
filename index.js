21da16c195553d4f096946e1717ac425c9d4cb32
unknown <contact.sspanda@gmail.com>
2022-09-25 17:06:26 +0530

    Add subtraction functionality and chaanged in index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const addTwoNum = require('./add')
+const subtractTwoNum = require('./substraction')
 
 console.log('This is my first GIT Test project')
 
 const res = addTwoNum(5,10)
 console.log('Added result is ' + res)
+
+const subres = subtractTwoNum(3,2)
+
+console.log(subres)
