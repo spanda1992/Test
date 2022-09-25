ae9623068ae84cb9ed9d3facd7d39e0b37a6e73e
unknown <contact.sspanda@gmail.com>
2022-09-25 17:09:46 +0530

    Added mulitplication functionality

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const addTwoNum = require('./add')
+const multiPlyTwoNum = require('./multiply')
 
 console.log('This is my first GIT Test project')
 
 const res = addTwoNum(5,10)
 console.log('Added result is ' + res)
+
+const multRes = multiPlyTwoNum(3,6)
+console.log(multRes)
---
+++ multiply.js
@@ -0,0 +1,5 @@
+const multiPlyTwoNum = (a,b) => {
+    return a * b
+}
+
+module.exports = multiPlyTwoNum
